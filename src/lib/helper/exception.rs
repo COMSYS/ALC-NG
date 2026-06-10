0035fcf84f5a59b6522489dadf940b87b1dd2539
David Schütte <me@dschuette.com>
2026-06-10 12:50:09 +0200

    fix(): fix force warning logging

--- src/lib/helper/exception.rs
+++ src/lib/helper/exception.rs
@@ -4,9 +4,9 @@ use log::warn;
 pub fn exception<T>(err: anyhow::Error, otherwise: T, target: &str, force: bool) -> Result<T> {
     if force {
         warn!(target: target, "{}", err);
-        warn!(target: target, "You can force the cleaner to continue cleaning using the --force / -f flag");
         Ok(otherwise)
     } else {
+        warn!(target: target, "You can force the cleaner to continue cleaning using the --force / -f flag");
         Err(err)
     }
 }
